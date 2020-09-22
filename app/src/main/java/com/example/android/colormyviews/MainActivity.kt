24e5db42c9c402fa3fe3d2b3137f4a88ed9a52ed
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-09-22 12:50:01 +0200

    set onclick listeners to textViews and constraint layout

--- app/src/main/java/com/example/android/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/android/colormyviews/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.android.colormyviews
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(
+            findViewById(R.id.textViewOne),
+            findViewById(R.id.textViewTwo),
+            findViewById(R.id.textViewThree),
+            findViewById(R.id.textViewFour),
+            findViewById(R.id.textViewFive),
+            findViewById(R.id.constraintLayout)
+        )
+
+        for (item in clickableViews)
+            item.setOnClickListener{view ->makeColored(view)}
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            R.id.textViewOne -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.textViewTwo -> view.setBackgroundColor(Color.GRAY)
+            R.id.textViewThree -> view.setBackgroundColor(Color.BLUE)
+            R.id.textViewFour -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.textViewFive -> view.setBackgroundColor(Color.BLUE)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
